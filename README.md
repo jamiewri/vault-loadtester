976471d0151881ba5da6a368f6afddf90f646d44
jamiewri <jamiewri@me.com>
2021-10-19 08:15:32 +1100

    rebase

---
+++ README.md
@@ -0,0 +1,69 @@
+# vault-loadtester
+This repo is a collection of python scripts that i use as a basic method to envoke a multithreaded load on Vault. There is limted to no error handeling, use at own risk.
+
+## Dependancies
+- Python3
+- pip
+
+## Activate
+```bash
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt
+```
+
+## Deactivate
+```bash
+deactivate
+```
+
+## Usage
+Start a Vault dev instance
+```bash
+vault server -dev -dev-root-token-id=dev
+```
+
+Set the env vars so `vault-loadtester` knows how to reach Vault
+```bash
+export VAULT_ADDR=http://127.0.0.1:8200
+export VAULT_TOKEN=dev
+```
+
+This script expects the following to be configured in Vault
+- The transit engine to be mounted at `/transit`
+- An encryption key to already be created and its name passed with `--key <keyname>`
+
+Run transit test
+```bash
+./vault-loadtester --key test --duration 10
+```
+
+Help menu
+```bash
+./vault-loadtester -h
+usage: vault-loadtest [-h] [--threads [THREADS]] [--key [KEY]] [--duration [DURATION]] [-v]
+
+Non-Function Load Testing of Vault
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --threads [THREADS]   How many threads should be used.
+  --key [KEY]           What is the name of the encryption key to use.
+  --duration [DURATION]
+                        Duration of the test
+  -v, --verbosity       Increase logging verbosity with more --vv
+```
+
+Example run
+```bash
+./vault-loadtester --key test --duration 10
+19-Oct-21 08:11:31 19474 WARNING - Log level set to WARNING
+Requests per second 555.0
+Number of Reqests 5550
+Number of 200s 5550
+Mean = 0.00687 seconds
+90%  = 0.00894 seconds
+95%  = 0.00954 seconds
+99%  = 0.01105 seconds
+```
+
---
+++ utils.py
@@ -0,0 +1,98 @@
+import logging
+import argparse
+import base64
+import random
+
+def startLogging(verbosity):
+    """
+    Initilize logging configuration to correct verbosity
+    """
+
+    if verbosity == 1:
+      logging.basicConfig(format='%(asctime)s %(process)d %(levelname)s - %(message)s',
+                          datefmt='%d-%b-%y %H:%M:%S',
+                          level=logging.INFO)
+      logging.info('Log level set to INFO')
+
+    elif verbosity == 2:
+      logging.basicConfig(format='%(asctime)s %(process)d %(levelname)s - %(message)s',
+                          datefmt='%d-%b-%y %H:%M:%S',
+                          level=logging.DEBUG)
+      logging.debug('Log level set to DEBUG')
+    else:
+      logging.basicConfig(format='%(asctime)s %(process)d %(levelname)s - %(message)s',
+                          datefmt='%d-%b-%y %H:%M:%S',
+                          level=logging.WARNING)
+      logging.warning('Log level set to WARNING')
+
+def argParser():
+    """
+    Parse argument passed on the command line, setting default where required.
+    """
+
+    parser = argparse.ArgumentParser(description='Non-Function Load Testing of Vault')
+  
+    parser.add_argument('--threads', 
+                        action='store',
+                        default=10,
+                        help='How many threads should be used.',
+                        nargs='?', 
+                        type=int,
+                        )
+
+    parser.add_argument('--key', 
+                        action='store', 
+                        default='randomKey',
+                        help='What is the name of the encryption key to use.',
+                        nargs='?', 
+                        type=str, 
+                        )
+
+    parser.add_argument('--duration', 
+                        action='store', 
+                        default=10,
+                        help="Duration of the test",
+                        nargs='?', 
+                        type=int, 
+                        )
+
+
+    parser.add_argument('-v',
+                        '--verbosity',
+                        action='count',
+                        default=0,
+                        help="Increase logging verbosity with more --vv",
+                        )
+    return parser
+
+def generateCreditCardNumbers(items):
+    """
+    Generate sudo credit cards numbers
+    """
+
+    data = []
+
+    for x in range(items):
+      item = []
+
+      for y in range(4):
+        block = random.randint(1000,9999)
+        block = str(block)
+        item.append(block)
+
+      creditCardNumber = '-'.join(item)
+      encodedCreditCardNumber = base64EncodeString(creditCardNumber)
+      data.append(encodedCreditCardNumber)
+
+    return data
+
+def base64EncodeString(data):
+    """
+    Return base64 coded string
+    """
+
+    dataBytes = data.encode('ascii')
+    base64Bytes = base64.b64encode(dataBytes)
+    base64String = base64Bytes.decode('ascii')
+  
+    return base64String
